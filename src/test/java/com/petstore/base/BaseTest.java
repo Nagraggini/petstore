6d6cbeb92a914368dd5c98a6b0ceda7715c05f36
Andrea Freud <62719085+Nagraggini@users.noreply.github.com>
2026-06-29 20:59:55 +0200

    feat: set logs and  create test for practising

--- src/test/java/com/petstore/base/BaseTest.java
+++ src/test/java/com/petstore/base/BaseTest.java
@@ -1,14 +1,48 @@
 package com.petstore.base;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import org.junit.jupiter.api.BeforeAll;
 
 import io.restassured.RestAssured;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
 
 public class BaseTest {
 
+    protected static PrintStream fileOut;
+
     @BeforeAll
-    static void setup() {
+    public static void setup() throws FileNotFoundException {
         RestAssured.baseURI = "https://petstore.swagger.io/v2";
+
+        File directory = new File("logs");
+        if (!directory.exists()) {
+            directory.mkdir();
+        }
+
+        String timeStamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd-HHmmss"));
+        String filePath = "logs/test-" + timeStamp + ".log";
+
+        // Az osztályszintű változó inicializálása
+        fileOut = new PrintStream(new FileOutputStream(filePath));
+
+        // Filterek beállítása egyszer
+        RestAssured.filters(
+                new RequestLoggingFilter(fileOut),
+                new ResponseLoggingFilter(fileOut));
+
     }
 
+    // Segédmetódus a log fájl "elválasztásához"
+    protected void logTestHeader(String testName) {
+        fileOut.println("\n" + "=".repeat(20));
+        fileOut.println("TESZT KEZDETE: " + testName);
+        fileOut.println("=".repeat(20) + "\n");
+    }
 }
--- src/test/java/com/petstore/pet/PetApiTest.java
+++ src/test/java/com/petstore/pet/PetApiTest.java
@@ -1,5 +1,6 @@
 package com.petstore.pet;
 
+import static org.hamcrest.Matchers.equalTo;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.DisplayName;
@@ -7,73 +8,108 @@ import org.junit.jupiter.api.Test;
 
 import com.petstore.base.BaseTest;
 
+import static io.restassured.RestAssured.baseURI;
+import static io.restassured.RestAssured.get;
 import static io.restassured.RestAssured.given;
 import io.restassured.response.Response;
 
 public class PetApiTest extends BaseTest {
 
-    /**
-     * Returns a single pet by id.
-     */
-    @Test
-    @DisplayName("Returns a single pet by id.")
-    void getPetByPetId() {
-        int petId = 3;
+        /**
+         * Trying it.
+         */
+        @Test
+        @DisplayName("Trying it.")
+        void tryTest() {
+                Response response = get("https://petstore.swagger.io/v2/pet/5");
+                response.getStatusCode();
+                response.getTime();
 
-        Response response = given()
-                .pathParam("petId", petId)
-                .when()
-                .get("/pet/{petId}");
+                System.out.println("response.getBody().asString(): " + response.getBody().asString());
+                // response.getBody().asString():
+                // {"id":2,"name":"Doggie","photoUrls":[],"tags":[],"status":"available"}
+                System.out.println("getStatusLine: " + response.getStatusLine());
+                System.out.println("content-type: " + response.getHeader("content-type"));
 
-        response.then()
-                .statusCode(200);
+                assertEquals(response.getStatusCode(), 200);
+        }
 
-        assertEquals(petId, response.jsonPath().getInt("id"));
-        assertEquals("bbre", response.jsonPath().getString("name"));
-        assertEquals("sold", response.jsonPath().getString("status"));
-        System.out.println("StatusCode: " + response.getStatusCode() + "; getTime: " + response.getTime());
-    }
+        /**
+         * 2 Trying it.
+         */
+        @Test
+        @DisplayName("2 Trying it.")
+        void try2Test() {
+                baseURI = "https://petstore.swagger.io/v2";
 
-    /**
-     * Returns a not found pet by id.
-     */
-    @Test
-    @DisplayName("Returns a not found pet by id.")
-    void getPetByPetIdNegativeTest() {
-        int petId = 0;
+                given().queryParam("status", "pending").then().statusCode(200).body("id[1]",
+                                equalTo("NewName")).log().all();
 
-        Response response = given()
-                .pathParam("petId", petId)
-                .when()
-                .get("/pet/{petId}");
+        }
 
-        response.then()
-                .statusCode(404);
+        /**
+         * Returns a single pet by id.
+         */
+        @Test
+        @DisplayName("Returns a single pet by id.")
+        void getPetByPetId() {
+                int petId = 5;
 
-        assertEquals(1, response.jsonPath().getInt("code"));
-        assertEquals("error", response.jsonPath().getString("type"));
-        assertEquals("Pet not found", response.jsonPath().getString("message"));
-    }
+                Response response = given()
+                                .pathParam("petId", petId)
+                                .when()
+                                .get("/pet/{petId}");
 
-    /**
-     * Returns a all pets by status.
-     */
-    @Test
-    @DisplayName("Returns a all pets by status.")
-    void getPetByPetStatus() {
-        String status = "pending";
+                response.then()
+                                .statusCode(200);
 
-        Response response = given()
-                .pathParam("status",
-                        status)
-                .when()
-                .get("/pet/findByStatus?status={status}");
+                assertEquals(petId, response.jsonPath().getInt("id"));
+                assertEquals("doggie", response.jsonPath().getString("name"));
+                assertEquals("string", response.jsonPath().getString("status"));
 
-        response.then()
-                .statusCode(200);
+                System.out.println("StatusCode: " + response.getStatusCode() + "; getTime: " + response.getTime());
+        }
 
-        assertTrue(response.jsonPath().getString("status").contains(status));
+        /**
+         * Returns a not found pet by id.
+         */
+        @Test
+        @DisplayName("Returns a not found pet by id.")
+        void getPetByPetIdNegativeTest() {
+                int petId = 0;
 
-    }
+                Response response = given()
+                                .pathParam("petId", petId)
+                                .when()
+                                .get("/pet/{petId}");
+
+                response.then()
+                                .statusCode(404);
+
+                assertEquals(1, response.jsonPath().getInt("code"));
+                assertEquals("error", response.jsonPath().getString("type"));
+                assertEquals("Pet not found", response.jsonPath().getString("message"));
+        }
+
+        /**
+         * Returns a all pets by status.
+         */
+        @Test
+        @DisplayName("Returns a all pets by status.")
+        void getPetByPetStatus() {
+                String status = "pending";
+
+                Response response = given()
+                                .pathParam("status",
+                                                status)
+                                .when()
+                                .get("/pet/findByStatus?status={status}");
+
+                response.then()
+                                .statusCode(200);
+
+                assertTrue(response.jsonPath().getString("status").contains(status));
+
+        }
 
 }
