ef4ed605624a567ba00b24e5a4f1c9c4b7587884
Andrea Freud <62719085+Nagraggini@users.noreply.github.com>
2026-07-15 18:28:03 +0200

    feat: create first test where get full list of available pets

--- src/test/java/com/petstore/api/petList/PetListTest.java
+++ src/test/java/com/petstore/api/petList/PetListTest.java
@@ -1,18 +1,39 @@
 package com.petstore.api.petList;
 
-import static org.hamcrest.Matchers.equalTo;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import org.junit.jupiter.api.DisplayName;
+import java.util.List;
+
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.notNullValue;
 import org.junit.jupiter.api.Test;
 
 import com.petstore.api.base.BaseApiTest;
 
-import static io.restassured.RestAssured.baseURI;
-import static io.restassured.RestAssured.get;
 import static io.restassured.RestAssured.given;
-import io.restassured.response.Response;
 
 public class PetListTest extends BaseApiTest {
+
+    /**
+     * TC1.1 Elérhető állatok listájának lekérése
+     * előfeltétel: json formátumú választ várunk
+     * lépések:
+     * Küldj GET kérést a /pet végpontra.
+     * elvárt eredmény:
+     * http státuszkód: 200
+     * a válasz egy lista
+     * a lista nem üres
+     * a lista elemei tartalmaznak “id” és “name” mezőket
+     */
+    @Test
+    void petListTest() {
+        String statusLvl = "available";
+
+        given().when().get("/pet/findByStatus?status={statusLvl}", statusLvl).then().statusCode(200).body("$",
+                notNullValue())// a válasz nem nullérték
+                .body("$", instanceOf(List.class)) // a válasz egy lista
+                .body("size()", greaterThan(0)) // a lista nem üres
+                .body("[0].id", notNullValue()) // az első elemnek van id mezője
+                .body("[0].name", notNullValue()); // az első elemnek van name mezője
+    }
 
 }
